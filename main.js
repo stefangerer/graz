dd6199d6d9a833f1a287216e9cc0dda6a8982597
Stefan Angerer <angerer.st@gmail.com>
2022-06-09 17:45:12 +0200

    Initial Push

---
+++ main.js
@@ -0,0 +1,44 @@
+
+let graz = {
+  lat: 47.05,
+  lng: 15.45,
+  title: "Graz"
+};
+
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [ graz.lat, graz.lng ],
+    zoom: 12,
+    layers: [
+        startLayer
+    ],
+});
+
+let layerControl = L.control.layers({
+    "BasemapAT Grau": startLayer,
+    "Basemap Standard": L.tileLayer.provider("BasemapAT.basemap"),
+    "Basemap High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+    "Basemap Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+    "Basemap Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+    "Basemap Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "Basemap Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
+    "Basemap mit Orthofoto und Beschriftung": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay"),
+    ])
+}).addTo(map);
+
+// Maßstab hinzufügen
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+let miniMap = new L.Control.MiniMap(
+    L.tileLayer.provider("BasemapAT"), {
+        toggleDisplay: true
+    }
+).addTo(map);
+
+// TODO: hier aus CSV konvertierte JSON Datei laden und anzeigen
+
